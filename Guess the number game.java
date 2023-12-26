6bd0ba3cdd5713f1ff8cdcf135bd26ad2366f41b
Paras Kumar <105094703+goldstone23@users.noreply.github.com>
2023-12-26 22:36:03 +0530

    Update and rename Guess the number game to Guess the number game.java

--- Guess the number game.java
+++ Guess the number game.java
@@ -1,5 +1,22 @@
 //chota project
-		 
+
+package Gsproject;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Gsproject {
+	public static void printme() {
+		System.out.println("gold stone");
+	}
+	public static void printit(String it) {
+		System.out.println(it);
+	}
+	public static void printu(int a, int b) {
+		int sum = a+b;
+		System.out.println(sum);
+	}
+	public static void main(String []args) {
 		 Scanner r=new Scanner(System.in); 
 		 int merano = (int) (Math.random()*100);
 		 int userno=0;
